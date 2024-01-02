580de9ea4fa602966603dcd9d45438b1b358df80
codeabuu <abdulngui718@gmail.com>
2024-01-02 14:10:26 +0000

    ES6 computed property names

---
+++ 8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
