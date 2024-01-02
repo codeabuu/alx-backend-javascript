e2dbee7a9d9723422d02885a0c49570b0cf33a54
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-01-02 12:53:55 -0500

    Update 6-string-interpolation.js

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,8 +6,5 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States
-          with a per capita personal income of ${budget.income}. As of 2015, San Francisco
-          proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
-
