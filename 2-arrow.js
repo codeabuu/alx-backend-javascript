abc9afbcdcd5f9e142a79a8980fc73a49aef7480
codeabuu <abdulngui718@gmail.com>
2024-01-02 13:38:05 +0000

    using ES6 arrow syntax of the function add

---
+++ 2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
