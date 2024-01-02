126d9bb5a747cf8245966bf92d61573cc1a0e698
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-01-02 12:50:44 -0500

    Update 1-block-scoped.js

--- 1-block-scoped.js
+++ 1-block-scoped.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    const task = true;
     const task2 = false;
   }
 
