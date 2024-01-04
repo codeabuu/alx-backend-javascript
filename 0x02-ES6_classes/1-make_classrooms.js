b73adec755ac1cc1f6a1497110bd2924a821ea0c
codeabuu <abdulngui718@gmail.com>
2024-01-04 13:13:46 +0000

    returning arrays of nos

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-    const r1 = new ClassRoom(19);
-    const r2 = new ClassRoom(20);
-    const r3 = new ClassRoom(34);
-    return [r1,r2,r3];
+  const r1 = new ClassRoom(19);
+  const r2 = new ClassRoom(20);
+  const r3 = new ClassRoom(34);
+  const las = [r1, r2, r3];
+  return (las);
 }
