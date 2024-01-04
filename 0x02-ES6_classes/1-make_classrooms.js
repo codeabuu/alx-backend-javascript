f31dd9706ab29cb162b16e7b0ce44ea9d70606d4
codeabuu <abdulngui718@gmail.com>
2024-01-04 13:10:58 +0000

    returning arrays of characters

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from "./0-classroom.js";
 
-function initializeRooms() {
+export default function initializeRooms() {
     const r1 = new ClassRoom(19);
     const r2 = new ClassRoom(20);
     const r3 = new ClassRoom(34);
