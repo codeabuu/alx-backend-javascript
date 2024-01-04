0fcf86f145af17c7d67001a8d8fdc4e812007aee
codeabuu <abdulngui718@gmail.com>
2024-01-04 13:06:15 +0000

    returning 3 classrooms with an array

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom.js";
+
+function initializeRooms() {
+    const r1 = new ClassRoom(19);
+    const r2 = new ClassRoom(20);
+    const r3 = new ClassRoom(34);
+    return [r1,r2,r3];
+}
