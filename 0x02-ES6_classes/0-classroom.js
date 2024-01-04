8eca95acd32065d2e4b6c1890493b651d3c30ed2
codeabuu <abdulngui718@gmail.com>
2024-01-04 12:45:21 +0000

    using constructor to compute max student size

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
