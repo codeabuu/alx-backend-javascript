7adfd10617b1cd9677a9fee5b84438abf97b3b9d
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-01-08 13:28:28 -0500

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 export default function getListStudents() {
   return [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
