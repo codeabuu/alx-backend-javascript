b72d96c3316c3f3279336ea13cd8b70ed94802d4
codeabuu <abdulngui718@gmail.com>
2024-01-08 18:55:02 +0000

    returns a boolean if all the elements in the array exist within the set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,9 @@
 export default function hasValuesFromArray(set, list) {
-  return list.every(element => set.has(element));
+  let arg = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      arg = false;
+    }
+  });
+  return arg;
 }
