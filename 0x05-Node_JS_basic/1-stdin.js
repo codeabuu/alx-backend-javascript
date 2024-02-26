21c40a4139bba0d7098142a5e834d0aaa2f1dedc
codeabuu <abdulngui718@gmail.com>
2024-02-26 19:32:47 +0000

    updates

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-const readline = require('readline')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`)
-});
-
-rl.on('close', () => {
-  console.log("This important software is now closing")
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
