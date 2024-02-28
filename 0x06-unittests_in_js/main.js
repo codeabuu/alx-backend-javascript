5a0744e441a6e65d7668b238c0fec775b04ae1f9
codeabuu <abdulngui718@gmail.com>
2024-02-28 21:39:03 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/main.js
@@ -0,0 +1,6 @@
+const calculateNumber = require("./1-calcul");
+
+console.log(calculateNumber('SUM', 1.4, 4.5));
+console.log(calculateNumber('SUBTRACT', 1.4, 4.5));
+console.log(calculateNumber('DIVIDE', 1.4, 4.5));
+console.log(calculateNumber('DIVIDE', 1.4, 0));
